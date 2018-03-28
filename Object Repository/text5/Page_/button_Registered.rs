4e2966719652ceb223019591724da9ac3f6a4407
YufengDu <37770123+YufengDu@users.noreply.github.com>
2018-03-29 00:06:16 +0800

    Create button_Registered.rs

---
+++ Object Repository/text5/Page_/button_Registered.rs
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_Registered</name>
+   <tag></tag>
+   <elementGuidId>b782d554-700d-4b37-9773-ce3398b9bcc8</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Registered</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/center[1]/a[1]/button[1]</value>
+   </webElementProperties>
+</WebElementEntity>
