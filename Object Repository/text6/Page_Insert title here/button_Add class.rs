2bc1bb35cfee898ce02bf93d13b1b24e5b89285c
YufengDu <37770123+YufengDu@users.noreply.github.com>
2018-03-29 00:10:38 +0800

    Create button_Add class.rs

---
+++ Object Repository/text6/Page_Insert title here/button_Add class.rs
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_Add class</name>
+   <tag></tag>
+   <elementGuidId>2ed65f21-8342-47bd-86e7-35f2218e46ba</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Add class</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/a[1]/button[1]</value>
+   </webElementProperties>
+</WebElementEntity>
