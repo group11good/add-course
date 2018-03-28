7011226fa3711de6ac6822192d83fe5d63765c29
RippleZYWL <37432788+RippleZYWL@users.noreply.github.com>
2018-03-28 22:37:53 +0700

    Add files via upload

---
+++ test7.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>test7</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-03-28T20:07:29</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>2a68e8ec-cfea-4d83-b251-9a895ed0a723</testSuiteGuid>
+   <testCaseLink>
+      <guid>629f9dc7-662c-40ee-a8fb-436a1314e561</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test7</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
