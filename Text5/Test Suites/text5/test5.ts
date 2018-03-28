5107798bb504fd98a0c559e66005ef4a2eab86f1
YufengDu <37770123+YufengDu@users.noreply.github.com>
2018-03-28 23:34:50 +0800

    Add files via upload

---
+++ Text5/Test Suites/text5/test5.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>test5</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-03-28T21:25:49</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>755b6338-c168-40c3-87d4-396080fd7da0</testSuiteGuid>
+   <testCaseLink>
+      <guid>109ed65b-f4ab-4cdd-9111-664f12576024</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test5</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
