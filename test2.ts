c28c8b31cc036fec0ce5edc46eabb58778ee2022
ZhangPengfei1 <37770646+ZhangPengfei1@users.noreply.github.com>
2018-03-28 21:10:33 +0700

    Add files via upload

---
+++ test2.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>test2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-03-28T21:23:42</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>a6589e26-e502-4b67-b9b2-99be90088b07</testSuiteGuid>
+   <testCaseLink>
+      <guid>07abe29e-1e67-4533-bda5-d0457d98133b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
