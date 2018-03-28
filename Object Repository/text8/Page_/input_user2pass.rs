b4fdc9b48aefad4d44c2496d7f089504cfd31333
YufengDu <37770123+YufengDu@users.noreply.github.com>
2018-03-29 00:13:47 +0800

    Add files via upload

---
+++ Object Repository/text8/Page_/input_user2pass.rs
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>input_user2pass</name>
+   <tag></tag>
+   <elementGuidId>89e58bf5-f83b-4838-849d-e27789ac6aa5</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>input</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>type</name>
+      <type>Main</type>
+      <value>password</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>name</name>
+      <type>Main</type>
+      <value>user2pass</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/center[1]/form[1]/table[1]/tbody[1]/tr[2]/td[2]/input[1]</value>
+   </webElementProperties>
+</WebElementEntity>
