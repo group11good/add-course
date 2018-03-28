b47ec697e6ccc7a5f4add4a0cd1fd5f9c4e53d0f
YufengDu <37770123+YufengDu@users.noreply.github.com>
2018-03-28 23:40:07 +0800

    Create test6.ts

---
+++ Text6/Test Suites/test6.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>test6</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-03-28T21:27:43</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>ad78ed72-f937-41b4-ad13-091b6b4bd4b7</testSuiteGuid>
+   <testCaseLink>
+      <guid>62535c91-589e-4422-afa0-9face43dc250</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test6 student add class</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
