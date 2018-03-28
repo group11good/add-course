8baa2d1445c838ec7b6e4e8b2b0d7ce071ee22c4
YufengDu <37770123+YufengDu@users.noreply.github.com>
2018-03-29 00:08:35 +0800

    Create button_Back to the login scree.rs

---
+++ Object Repository/text5/Page_Insert title here/button_Back to the login scree.rs
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_Back to the login scree</name>
+   <tag></tag>
+   <elementGuidId>6da86ada-2011-4905-b467-749f5d7722b8</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Back to the login screen</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/a[1]/button[1]</value>
+   </webElementProperties>
+</WebElementEntity>
